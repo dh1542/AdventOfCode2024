1bf8061863dfbadeb70cc07c4ed92a8abb52d524
Dominik Hoffmann <dominik.hoffmann@iso-gruppe.com>
2024-12-02 10:15:35 +0100

    Initial commit

---
+++ src/Day1/Day1.java
@@ -0,0 +1,19 @@
+package Day1;
+
+import java.util.List;
+
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+    private static List<List<Integer>> alignLists(List<Integer> listA, List<Integer> listB){
+        return null;
+    }
+
+
+    
+
+
+}
